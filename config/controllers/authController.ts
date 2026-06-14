2a6d7d82f02404e338a2bdd33b80078d8c9e4c9c
Jorge Matos <jisapjisap@gmail.com>
2026-06-14 11:42:02 +0200

    feat: implement authentication controller and install bcrypt, jsonwebtoken, and multer dependencies

---
+++ config/controllers/authController.ts
@@ -0,0 +1,62 @@
+import { Request, Response } from "express";
+import { prisma } from "../prisma.js";
+import bcrypt from "bcrypt"
+import { hasSubscribers } from "node:diagnostics_channel";
+import jwt from "jsonwebtoken";
+
+//Generate JWT Token
+const generateToken = (id: string) => {
+  return jwt.sign({ id }, process.env.JWT_SECRET as string, { expiresIn: "30d" })
+}
+
+//Check if user is admin
+const getAdminStatus = (email: string | null | undefined): boolean => {
+  if (!email) return false;
+
+  const adminEmails = process.env.ADMIN_EMAILS
+    ? process.env.ADMIN_EMAILS.split(",").map((e) => e.trim().toLowerCase())
+    : [];
+
+  return adminEmails.includes(email.toLowerCase());
+}
+
+// Register
+// POST /api/auth/register
+export const register = async (req: Request, res: Response) => {
+  const { name, email, password } = req.body;
+
+  if (!name || !email || !password) {
+    return res.status(400).json({ message: "All fields are required" });
+  }
+
+  const existingUser = await prisma.user.findUnique({
+    where: { email: email.toLowercase() }
+  });
+
+  if (existingUser) {
+    return res.status(400).json({ message: "User already exists with this email" });
+  }
+
+  const hashedPassword = await bcrypt.hash(password, 10);
+
+  const user = await prisma.user.create({
+    data: {
+      name, email: email.toLowercase(),
+      password: hashedPassword
+    }
+  })
+
+  const token = generateToken(user.id);
+
+  const userData: any = { ...user };
+  delete userData.password
+  userData.isAdmin = getAdminStatus(userData.email);
+
+  res.status(201).json({
+    message: "User registered successfully",
+    user: userData,
+    token
+  });
+
+
+}
